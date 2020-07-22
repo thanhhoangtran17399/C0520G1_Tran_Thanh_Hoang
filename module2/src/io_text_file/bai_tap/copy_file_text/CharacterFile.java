dd1984ce78103d22030e9ed68c9932789a510398
thanhhoangtran17399 <thanhhoangtran17399@gmail.com>
2020-07-22 08:47:12 +0700

    BaiTap

---
+++ module2/src/io_text_file/bai_tap/copy_file_text/CharacterFile.java
@@ -0,0 +1,45 @@
+package io_text_file.bai_tap.copy_file_text;
+
+import java.io.*;
+
+public class CharacterFile {
+    static String str = "";
+
+    public static void readFile() {
+        File file = new File("D:\\C0520G1_Tran_Thanh_Hoang\\module2\\src\\io_text_file\\bai_tap\\copy_file_text\\test_reader");
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                str += line;
+            }
+            bufferedReader.close();
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeFile() {
+        File file2 = new File("D:\\C0520G1_Tran_Thanh_Hoang\\module2\\src\\io_text_file\\bai_tap\\copy_file_text\\test2");
+        try {
+            FileWriter fileWriter = new FileWriter(file2);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(str);
+
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        readFile();
+        writeFile();
+    }
+}
+
